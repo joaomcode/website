2c5ddd14f0b6cd3179b1a1f17240be191394d935
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-13 10:09:40 -0300

    create: add about page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Content } from './style';
+
+const About: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>About</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default About;
