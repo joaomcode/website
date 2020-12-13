1779879a75d7ab997c2d026637179a6b2ecf0703
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-13 10:10:14 -0300

    create: add portfolio page

---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Content } from './style';
+
+const Portfolio: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>Portfolio</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default Portfolio;
