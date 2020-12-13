e5abd2b9d9cbd8ca428cc54fef32764797ebd8c0
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-13 10:09:52 -0300

    create: add contact page

---
+++ src/pages/Contact/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Content } from './style';
+
+const Contact: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>Contact</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default Contact;
