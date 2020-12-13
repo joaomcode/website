e6e4a63e8b06607bebd8845e4e702cb85cef65b4
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-13 10:11:47 -0300

    mod: call header, routes and globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = () => <h1>Hi There!</h1>;
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <Header />
+    <Routes />
+
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
