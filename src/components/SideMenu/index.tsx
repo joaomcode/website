35a7759c1e92eb2400f67795b2e9f4ae96d5807c
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-17 11:00:40 -0300

    mod: adjust responsive menu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FiAlignJustify, FiX } from 'react-icons/fi';
 
-import { Container } from './style';
+import { Container, MenuBar } from './style';
 
 const SideMenu: React.FC = () => {
+  const [hide, setHide] = useState(true);
+
+  const handleClick = () => {
+    setHide(!hide);
+  };
+
+  const activeStyle = {
+    color: '#28b1e7',
+    listStyle: 'none',
+  };
+
+  const menuProps = [
+    { path: '/', label: 'Home' },
+    { path: '/portfolio', label: 'Portfólio' },
+    { path: '/about', label: 'Sobre' },
+    { path: '/contact', label: 'Contato' },
+  ];
+
   return (
     <Container>
-      <h1>Hi</h1>
+      <button type="button" onClick={handleClick}>
+        {hide ? <FiAlignJustify /> : <FiX />}
+      </button>
+
+      {!hide ? (
+        <MenuBar>
+          {menuProps.map(menu => (
+            <NavLink to={menu.path} exact activeStyle={activeStyle}>
+              <span>{menu.label}</span>
+            </NavLink>
+          ))}
+        </MenuBar>
+      ) : (
+        <span style={{ display: 'none' }}>a</span>
+      )}
     </Container>
   );
 };
--- src/components/SideMenu/style.ts
+++ src/components/SideMenu/style.ts
@@ -5,5 +5,30 @@ export const Container = styled.nav`
 
   @media screen and (max-width: 667px) {
     display: flex;
+
+    button {
+      font-size: 1.5em;
+      margin-top: 3px;
+      border: none;
+      color: #1e427f;
+    }
+
+    button:hover {
+      color: #28b1e7;
+    }
+  }
+`;
+
+export const MenuBar = styled.nav`
+  a {
+    text-decoration: none;
+    display: flex;
+    flex-direction: column;
+
+    color: #1e427f;
+  }
+
+  a:hover {
+    color: #28b1e7;
   }
 `;
