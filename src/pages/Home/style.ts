af6427fd4988e143bb013b669652ee687c512518
joaomcode <joaomarcelo.co@hotmail.com>
2020-12-21 09:29:03 -0300

    mod: remove content
    definition: content was removed and added on PagesContainer in global

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,9 +1,3 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
-
-export const Content = styled.div`
-  max-width: 80%;
-  justify-content: center;
-  margin: 0 10%;
-`;
